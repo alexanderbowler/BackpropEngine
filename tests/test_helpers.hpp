7f5c36e166c0a675550030d9a2cd8efdcf66aa99
Alex Bowler <albowler@umich.edu>
2025-07-06 20:23:47 -0400

    Fix nitpick removing commented out test_helper function which has been moved

--- tests/test_helpers.hpp
+++ tests/test_helpers.hpp
@@ -3,22 +3,4 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-
-// template <typename T>
-// void backprop_function_test(backprop::Function<T>& fn){
-//     T small_addition = 0.00001;
-//     T orig_output = fn.output_->item();
-//     // std::cout<<"Orig output: "<<orig_output<<"\n";
-
-//     fn.backward();
-//     for(std::shared_ptr<backprop::TensorImpl<T>> parent: fn.parents){
-//         T orig_parent_val = parent->item();
-//         parent->set(orig_parent_val + small_addition);
-//         fn.forward();
-//         // std::cout<<"parent "<<parent->item()<<"\n";
-//         // std::cout<<"Modded output: "<<fn.output_->item()<<"\n";
-//         T gradient = (fn.output_->item() - orig_output) / small_addition;
-//         EXPECT_NEAR(parent->grad(), gradient, 0.05);
-//         parent->set(orig_parent_val);
-//     }
-// }
+// Currenlty Not used the Function helper is in the Tensor Class
